fbba6c8c0b38db702159a1634bb515852a5e0463
HarshTiwari1131 <147716113+HarshTiwari1131@users.noreply.github.com>
2023-11-29 22:05:38 +0530

    Update category_manage.php

--- category_manage.php
+++ category_manage.php
@@ -8,20 +8,16 @@
         form {
             margin: auto;
             margin-top: 50px;
-            width: 500px;
+            width: 80%; /* Adjusted for responsiveness */
+            max-width: 500px;
             padding: 30px;
-            border: 1px solid black;
+            background-color: #90ee90; /* Light green background color */
+            border: 10px solid black; /* Solid black border with a size of 10 */
             border-radius: 5px;
         }
 
-        input {
-            width: 90%;
-            padding: 10px;
-            margin-bottom: 10px;
-        }
-
-        textarea {
-            width: 90%;
+        input, textarea {
+            width: 100%;
             padding: 10px;
             margin-bottom: 10px;
         }
@@ -29,6 +25,7 @@
         .buttons {
             display: flex;
             justify-content: center;
+            gap: 10px;
         }
 
         input[type="checkbox"] {
@@ -43,33 +40,48 @@
             width: 100px;
             border: 1px solid black;
             border-radius: 5px;
-            color: white;
             font-weight: 700;
+            cursor: pointer;
+            transition: background-color 0.3s ease; /* Add transition effect */
         }
 
         button[type="reset"] {
-            background-color: red;
+            background-color: #ff6961; /* Light red */
+        }
+
+        button[type="reset"]:hover {
+            background-color: #ff4741; /* Darker red on hover */
         }
 
         button[type="submit"] {
-            background-color: green;
+            background-color: #90ee90; /* Light green */
+        }
+
+        button[type="submit"]:hover {
+            background-color: #7ace7a; /* Darker green on hover */
+        }
+
+        @media screen and (max-width: 600px) {
+            form {
+                width: 100%; /* Full width on smaller screens */
+            }
         }
     </style>
 </head>
 <body>
-<form action="" method="post" enctype="multipart/form-data">
-    <label for="name">Category Name*</label><br>
-    <input type="text" name="name" id="name" placeholder="Enter Category name">
-    <label for="description">Description</label><br>
-    <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter Description"></textarea>
-    <label for="image">Image</label><br>
-    <input type="file" name="image" id="image">
-    <label for="status">Status</label>
-    <input type="checkbox" name="status">
-    <div class="buttons">
-        <button type="reset">CANCEL</button>
-        <button type="submit">SAVE</button>
-    </div>
-</form>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="name">Category Name*</label><br>
+        <input type="text" name="name" id="name" placeholder="Enter Category name">
+        <label for="description">Description</label><br>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Enter Description"></textarea>
+        <label for="image">Image</label><br>
+        <input type="file" name="image" id="image">
+        <label for="status">Status</label>
+        <input type="checkbox" name="status">
+        <div class="buttons">
+            <button type="reset">CANCEL</button>
+            <button type="submit">SAVE</button>
+        </div>
+    </form>
 </body>
 </html>
